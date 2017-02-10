8b7adf422ef41cd81068c84bee8bc00827eace5b
chmodder <hotmailgateway@gmail.com>
2017-02-10 11:09:18 +0100

    Created basic server application.

--- TCPChatServer/Program.cs
+++ TCPChatServer/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TCPChatServer
 {
-    class Program
+    class TcpServer
     {
         static void Main(string[] args)
         {
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+            Console.WriteLine($"Server started");
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+
+            Console.WriteLine($"Server activated");
+
+
+
         }
     }
 }

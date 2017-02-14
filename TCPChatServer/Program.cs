6320b1c6f4fffc96c933c84d7a874ccaf9a377b1
chmodder <hotmailgateway@gmail.com>
2017-02-14 11:06:20 +0100

    Added 2 way communication, so it has both server and client compatability

--- TCPChatServer/Program.cs
+++ TCPChatServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
@@ -11,14 +12,30 @@ namespace TCPChatServer
     {
         static void Main(string[] args)
         {
+
+            #region my 2 way server code
+            //set up server
             TcpListener serverSocket = new TcpListener(6789);
             serverSocket.Start();
             Console.WriteLine($"Server started");
-
             TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-
             Console.WriteLine($"Server activated");
 
+            Stream ns = connectionSocket.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader sr = new StreamReader(ns);
+
+            while (true)
+            {
+                //receive message  
+                string receivedMsg = sr.ReadLine();
+                Console.WriteLine($"Client: {receivedMsg}");
+                //send message
+                string sendMsg = Console.ReadLine();
+                sw.WriteLine(sendMsg);
+                sw.AutoFlush = true;
+            }
+            #endregion
 
 
         }
